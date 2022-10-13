cfad2162fa9c2e09f048e5bd9fdc94d4e1774028
bongo84 <41794045+bongo84@users.noreply.github.com>
2022-10-13 21:42:11 +0100

    adding base setup for framework

---
+++ notes.js
@@ -0,0 +1,14 @@
+/**
+ * This is going to be a skeleton framework for Javascript test framework that will use the 
+ * BDD style and use cucumber Gherkin Selenium & Supertest for Api testing
+ * 
+ * 1- Inital setup needs to follow the Learn BDD framework book and use the set up and hooks needed 
+ * for this
+ * 2- Needs adequete logging and reporting of test results
+ * 3- Form of reporting that is built into the framework
+ * 4- Scripts that run adequete test commands
+ * 5- The ability for this to be run in a CI environment
+ * 6- Tests need to be spec on a trello board and fleshed out on a 
+ * test website
+ * 7- Yarn install and sort the sudo permissions for running updates
+ */
